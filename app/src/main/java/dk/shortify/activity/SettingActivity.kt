5c166af2cab3bfafcde788fdc2d00e82ddfb065b
박현준 <evoluti0n@hanyang.ac.kr>
2020-01-30 18:34:21 +0900

    license dialog

--- app/src/main/java/dk/shortify/activity/SettingActivity.kt
+++ app/src/main/java/dk/shortify/activity/SettingActivity.kt
@@ -10,6 +10,10 @@ import android.content.Intent
 import android.net.Uri
 import android.view.MenuItem
 import androidx.lifecycle.lifecycleScope
+import de.psdev.licensesdialog.LicensesDialog
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20
+import de.psdev.licensesdialog.model.Notice
+import de.psdev.licensesdialog.model.Notices
 import dk.shortify.R
 import dk.shortify.db.DB
 import kotlinx.coroutines.Dispatchers
@@ -40,7 +44,11 @@ class SettingActivity : AppCompatActivity() {
             addPreferencesFromResource(dk.shortify.R.xml.settings_preference)
 
             val historyClear: PreferenceScreen? = findPreference("history_clear")
+
             val homepage: PreferenceScreen? = findPreference("homepage")
+            val other: PreferenceScreen? = findPreference("other_app")
+
+            val license: PreferenceScreen? = findPreference("license")
             val version: PreferenceScreen? = findPreference("version")
 
             historyClear?.setOnPreferenceClickListener {
@@ -61,7 +69,38 @@ class SettingActivity : AppCompatActivity() {
                 true
             }
 
+            other?.setOnPreferenceClickListener {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/dev?id=5704360550255011002")
+                    )
+                )
+                true
+            }
+
+            license?.setOnPreferenceClickListener {
+                showLicense()
+            }
+
             version?.summary = getAppVersionName()
+        }
+
+        private fun showLicense(): Boolean{
+            val notices = Notices().apply {
+                addNotice(Notice("Retrofit", "https://square.github.io/retrofit", "Copyright 2013 Square, Inc.", ApacheSoftwareLicense20()))
+                addNotice(Notice("Gson", "https://github.com/google/gson", "Copyright 2008 Google Inc.", ApacheSoftwareLicense20()))
+                addNotice(Notice("Splitties", "https://github.com/LouisCAD/Splitties", "", ApacheSoftwareLicense20()))
+                addNotice(Notice("no-internet-layout", "https://github.com/MohamedWessam/no-internet-layout", "Copyright 2019 Mohamed Wessam.", ApacheSoftwareLicense20()))
+            }
+
+            LicensesDialog.Builder(context)
+                .setNotices(notices)
+                .setIncludeOwnLicense(true)
+                .build()
+                .show()
+
+            return true
         }
 
         private fun getAppVersionName(): String {
