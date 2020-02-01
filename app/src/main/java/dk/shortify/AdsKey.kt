77ae1a651c7a49b1a5212e21d73633560a8519ec
HyeonJun Park <dookong727@gmail.com>
2020-02-01 23:12:38 +0900

    Delete AdsKey.kt

--- app/src/main/java/dk/shortify/AdsKey.kt
+++
@@ -1,9 +0,0 @@
-package dk.shortify
-
-interface AdsKey {
-    companion object{
-        const val FACEBOOK = "205936343898527_205937127231782"
-        const val FACEBOOK2 = "205936343898527_205967933895368"
-        const val ADMOB = "ca-app-pub-2259613318357780/7958469545"
-    }
-}
